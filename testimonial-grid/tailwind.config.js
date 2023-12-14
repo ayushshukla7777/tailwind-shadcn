c524520c3d3867c49e4276d0b20656db2271651d
Ayush Aryan <ayushshukla7777@gmail.com>
2023-12-14 07:57:11 +0530

    Add testimonial-grid project files and styles

---
+++ testimonial-grid/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
